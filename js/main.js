e92837a29a8757183730921e886c0c6c51ac29e3
knightf <knightf86@gmail.com>
2015-04-11 19:36:18 -0400

    creatine plugin implemented

--- js/main.js
+++ js/main.js
@@ -1,35 +1,46 @@
-var game = {};		//whole game scope object
 
-game.queue.bedRoomQueue = new createjs.LoadQueue();
+var game = {
+	"loaders" : {
+		"bedRoomQueue" : new createjs.LoadQueue(),
+		"bedroomComplete" : function(){
+			var queue = game.loaders.bedRoomQueue,
+			    image = queue.getResult("bg"),
+			    bedroom = new creatine.Scene();
+			bitmap = new createjs.Bitmap(image);
+			bedroom.addChild(bitmap);
+			bitmap.x = 0;
+			bitmap.y = 0;
+			game.director.replace(bedroom);
+		},
+	},
 
- bedRoomQueue.on("complete", bedroomComplete, this);
+	"init" : function(){
+		this.canvas = document.getElementById('stage');
+		this.stage = new createjs.Stage(this.canvas);
+		this.director = new creatine.Director(this.stage);
+		// enable touch interactions if supported on the current device:
+		createjs.Touch.enable(game.stage);
+		// enabled mouse over / out events
+		game.stage.enableMouseOver(10);
+		game.stage.mouseMoveOutside = true;
 
- bedRoomQueue.loadManifest([
-     {id: "bg", src:"./images/bedroom.png"},
-     {id: "drawer1", src:"./images/bedroom_drawer1.png"},
-     {id: "drawer2", src:"./images/bedroom_drawer2.png"},
-     {id: "painting", src:"./images/bedroom_painting.png"},
- ]);
+		createjs.Ticker.setFPS(60);
+		createjs.Ticker.addEventListener('tick', onTick);
 
-function bedroomComplete() {
-	var image = bedRoomQueue.getResult("bg");
-	
-	var container = new createjs.Container();
-	game.stage.addChild(container);
-	bitmap = new createjs.Bitmap(image);
-	container.addChild(bitmap);
-	bitmap.x = 0;
-	bitmap.y = 0;
-	game.stage.update();
-	bitmap.on("click", function (evt){alert('s')});
+		this.loaders.bedRoomQueue.on("complete", game.loaders.bedroomComplete, this);
+		this.loaders.bedRoomQueue.loadManifest([
+			{id: "bg", src:"./images/bedroom.png"},
+			{id: "drawer1", src:"./images/bedroom_drawer1.png"},
+			{id: "drawer2", src:"./images/bedroom_drawer2.png"},
+			{id: "painting", src:"./images/bedroom_painting.png"},
+		]);
+	},
+};		//whole game scope object
+
+function onTick() {
+    game.stage.update();
 }
 
-function init() {
-	game.canvas = document.getElementById('stage');
-	game.stage = new createjs.Stage(game.canvas);
-	// enable touch interactions if supported on the current device:
-	createjs.Touch.enable(game.stage);
-	// enabled mouse over / out events
-	game.stage.enableMouseOver(10);
-	game.stage.mouseMoveOutside = true;
-}
+document.addEventListener("DOMContentLoaded", function(event){
+	game.init();
+});

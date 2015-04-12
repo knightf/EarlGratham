1c137c2135cf74a822750d677e83056ea806863a
chw120 <chw120@pitt.edu>
2015-04-12 15:04:11 -0400

    script

---
+++ js/game.dialogue.js
@@ -0,0 +1,67 @@
+game.dialogue = {
+
+	"script" : {
+
+	"bedroom" : {
+		"diary1" : "Oh, this is Elizabeth’s diary.I can definitely find something in this…… \n
+		\"Date: Sep 9th, 1883 \n
+        Today we married! We love each other so much and today we finally got married and received the God’s blessing. I’m the happiest and luckiest woman in the world…...\"\n
+ 		\"Date: Dec 12th, 1883\n
+        Oh no! Charlie has had an intermittent amnesia symptom for several years and I have not even found out. When the symptom breaks out, he forgets everything and he does not know who I am. I have to constantly help him to recover his memory…..\"\n
+		\"Date: Sep 9th, 1884\n
+        Today is our first wedding anniversary, but we did not have any celebration. I expected there would be roses ,gifts and everything would be fantastic. Actually, Charlie’s symptom break out again…… He forgot me and he forgot our anniversary. From last year to now, his symptom break out several times and I got tired of this……\"\n
+		\"Date: Oct 4th, 1884\n
+        I have to admit that Jack has real talent in painting and I was wrong. He sent us his new painting as a gift and I am impressed by his touch on the canvas. Julia is so lucky to marry him…...\"\n
+        Part of this diary is missing. The last one is 2 years old.",
+		
+		"diary2" : "Another part of Elizabeth’s diary.\n
+		\"Date: Jan 20th, 1885\n
+		Pray to the Lord. I am pregnant with Charlie’s baby. I just can not wait to bring this new life to the world and introduce all the beautiful things to him.\"\n
+		\"Date:Sep 16th, 1885\n
+		It’s a boy! Charlie and I decide to name him Henry.\"\n
+		\"Date:Oct 18th, 1885\n
+		Henry left us. I don’t how to live without him.\"\n
+		Oh no...no! My poor baby! [Crying] Why the Lord has to do this to my family?! I don’t want to read this ever again.",
+
+		"diary3" : "Looks like here are more diaries.\n
+		\"Date: Mar 25th, 1886\n
+		I have no idea what Charlie has been doing lately, he seldomly talks to me now. However, I think Jack is a better listener. He can really get me.\"\n
+		\"Date: April 9th, 1886\n
+		Today a dangerous idea came to me: what if I leave this broken home and go with Jack? Why I have to stay at this place and go through the sadness over and over again with Charlie? His symptom broke out his morning again. I have to tell him we had a baby and we lost him. I don’t want to go through this again.\"\n
+		…… I don’t know what to say now.",
+		
+		"newsclipping" : "\"Forthcoming Wedding -- Earl C. Grantham and Miss E. Green\"\n
+	    \"The engagement is announced between Charlie, son of Ethan and Jane Grantham, and Elizabeth, daughter of Alexandar and Nancy Green.\"",
+		"painting" : "I miss my wife so much!"
+	},
+	
+    "livingroom" : {
+    	"Jackpainting" : "\"This is a piece of painting named “Summer Night” draw by Jack Miller——a nova artist in England, who is the husband of Julia Miller. Jack and Julia are friends of Charlie and Elizabeth.\"",
+    	"keybabyroom" : "It looks like an ordinary key...Maybe it works somewhere.\"",
+    	"flyer" : "Eeeh? What is this?... Wait! We seems to have visited there…\" Best holiday resort in the world ever\n Be the first one to own all of these — beautiful scenes, fresh air, brand new accommodation…\n Looking forward to your visit\"",
+  		"parasol" : "This is the parasol Elizabeth carried all the time. Oh, I miss her so much!"
+
+    },
+
+    "study" : {
+    	"newsyesterday" : "\"Great Success of Dr. Thomas' New Therapy [Nov 23rd, 1886] —— A young german physician has proven his new dicovery important with memory disorder patients. ‘It all relies on this new chemical I found in the lab 2 years ago’, Thomas said, ‘I name it ephedrine.’ Patients who received this nova medicine claim that they all feel more clear with their mind and some of the systoms disappear quickly. Some research indicates that this chemical is poisionous. Yet nobody cares as long as this panacea works.\"",
+    	"invitationletter" : "\"Dear Charlie,\n We're sorry to hear that our lovely angel Henry has left us and been in heaven. He must be having fun with other angels there now.  We know you're still grieving, especially Elizabeth, hear that she seems to be suffering from depression. We mourn Henry's passing too, however, we think it's more important that life still needs go on. We just got a resort's flyer, the introduction of this new holiday resort sounds wonderful. Therefore, we want to invite both of you visit there with us, to get some fresh air and have courage for your future lives. You're still young and will definitely have much more children. And I can talk with Elizabeth to help her get out of depression. After all, some topics are much easier for women to discuss.\n Looking forward to hearing from you!\n Best wishes,\n Yours Sincerely, Julie\"",
+		"charlietelegraph" : "\"22-3675\n EM66545\n 1885-11-13\n Charlie Gratham\n  No. 6 Fifth Ave, London\n WE REFER TO YOUR MT754 DATED NOVEMBER 10TH, 1885 AND OBSERVE THAT YOU HAVE ERRONEOUSLY C; AIMED GBP56630 INSTEAD OF GBP55250.00. THE DIFFERENCE OF GBP380 BEING OUR AMENTMENT COMMISSION’S DEMANDED VIDE OUR MT707 DATED OCTUBER 10TH 1885 WHICH YOU HAVE OMITTED TO COMPUTE. KINDLY ARRANCE TO CREDIT OUR ACCOUNT URGENTLY FOR GBP395 BEING COMMISSION FOR GBP15 BEING COST OF THIS SWIFT.\n  Edward\n A company\"",
+		"wastontelegraph" : "\"Dear Charlie,\n Scotland Yard Detective just found a new witness, Lily. According to her testimony, she saw Elizabeth and Jack had a fierce argument on that cliff on the night of Elizabeth death. We go all out to investigate this case and if there is any update, we will notify you at once.\"",
+		"casefile" : "\"Date: Nov 12th, 1885\n Charlie with his wife Elizabeth and Julie with her husband Jack came to the new holiday resort.\n
+		Date: Nov 13th, 1885\n They went out for sightseeing together all day long, and later that afternoon on the way went back, Elizabeth left alone to look for her lost parasol.\n
+		Date: Nov 14th, 1885\n Elizabeth's body was found under a cliff by a jogger \n
+		Date: Nov 21th, 1885\n After one week’s investigation, without any clear evidence and suspect, we assume that this is a case of suicide according to Elizabeth's mental state before her death.\"",
+		"policephoto" : "My poor Elizabeth, what have you been through? Hold on, there are some strange...What on earth is wrong with this?",
+		"diagnosis" : "\"Patient Name: Charlie Gratham\n Symptom Description: This patient has a relatively interesting symptoms. He loses his short-term memories irregularly and with certain incitation, he can successfully recover them. I assume that this patient is suffering from intermittent amnesia. The first period of the treament can hardly be considered successful. Though with the ephedrine the time spent on recoverying is shorten, the symptom is not relieved.\n Sign: Sean Thomas\""
+    }
+
+    "unknown" : {
+    	"necklace" : "This is Elizabeth's favorite jewelry, she always wore it.",
+    	"souvenirpic" : "What a splendid memory! Who exactly killed my wife?"
+    }
+}
+
+
+
+}

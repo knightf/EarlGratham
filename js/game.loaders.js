6c5bb1765cc89c7e48cc0cc09ca799db26e4160d
knightf <knightf86@gmail.com>
2015-04-12 10:12:33 -0400

    application unitized

---
+++ js/game.loaders.js
@@ -0,0 +1,72 @@
+game.loaders = {
+	//init
+	"init" : function(){
+		//bedroom scene
+		game.loaders.bedRoomQueue.on("complete", game.loaders.bedroomComplete, this);
+		game.loaders.bedRoomQueue.loadManifest([
+			{id: "bg", src:"./images/bedroom.png"},
+			{id: "drawer1", src:"./images/bedroom_drawer1.png"},
+			{id: "drawer2", src:"./images/bedroom_drawer2.png"},
+			{id: "painting", src:"./images/bedroom_painting.png"},
+		]);
+		//babyroom scene
+		game.loaders.babyRoomQueue.on("complete", game.loaders.babyroomComplete, this);
+		game.loaders.babyRoomQueue.loadManifest([
+			{id: "bg", src:"./images/babyroom.png"},
+			{id: "door1", src:"./images/babyroom_door1.png"},
+			{id: "door2", src:"./images/babyroom_door2.png"},
+			{id: "painting", src:"./images/babyroom_painting.png"},
+		]);
+	},
+	//bedroom scene
+	"bedRoomQueue" : new createjs.LoadQueue(),
+	"bedroomComplete" : function(){
+		var queue = game.loaders.bedRoomQueue,
+		    bedroom = new creatine.Scene(),
+		    bg, painting, drawer1, drawer2;
+		
+		bg = queue.getResult("bg"),
+		bgImg = new createjs.Bitmap(bg);
+		bedroom.addChild(bgImg);
+		bgImg.x = 0;
+		bgImg.y = 0;
+
+		painting = queue.getResult("painting");
+		paintingImg = new createjs.Bitmap(painting);
+		bedroom.addChild(paintingImg);
+		paintingImg.x = 208;
+		paintingImg.y = 51;
+
+		drawer1 = queue.getResult("drawer1");
+		drawer1Img = new createjs.Bitmap(drawer1);
+		bedroom.addChild(drawer1Img);
+		drawer1Img.x = 678;
+		drawer1Img.y = 309;
+
+		drawer2 = queue.getResult("drawer2");
+		drawer2Img = new createjs.Bitmap(drawer2);
+		bedroom.addChild(drawer2Img);
+		drawer2Img.x = 678;
+		drawer2Img.y = 339;
+		
+		//store the scene
+		game.sceneList['bedroom'] = bedroom;
+	},
+	//babyroom scene
+	"babyRoomQueue" : new createjs.LoadQueue(),
+	"babyroomComplete" : function(){
+		var queue = game.loaders.babyRoomQueue,
+		    babyroom = new creatine.Scene(),
+		    bg, painting, door1, door2;
+		
+		var bg = queue.getResult("bg"),
+		bgImg = new createjs.Bitmap(bg);
+		babyroom.addChild(bgImg);
+		bgImg.x = 0;
+		bgImg.y = 0;
+
+		var painting = queue.getResult("painting");
+		paintingImg = new createjs.Bitmap(painting);
+		babyroom.addChild(paintingImg);
+	},
+};
---
+++ js/game.main.js
@@ -0,0 +1,28 @@
+var game = this.game || {};
+
+game.init = function(){
+	this.canvas = document.getElementById('stage');
+	this.stage = new createjs.Stage(this.canvas);
+	this.director = new creatine.Director(this.stage);
+	//enable touch interactions if supported on the current device:
+	createjs.Touch.enable(game.stage);
+	//enabled mouse over / out events
+	game.stage.enableMouseOver(10);
+	game.stage.mouseMoveOutside = true;
+	//force to refresh the canvas
+	createjs.Ticker.setFPS(60);
+	createjs.Ticker.addEventListener('tick', onTick);
+	//load manifest
+	game.loaders.init();
+};
+
+//an empty object to store the scenes for latter access
+game.sceneList = {};
+
+function onTick() {
+    game.stage.update();
+}
+
+document.addEventListener("DOMContentLoaded", function(event){
+	game.init();
+});
